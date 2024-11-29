03fa3440b364a9649bade8f07e5c3c08e7538d93
Rignu1503 <rigoberto.miranda1503@gmail.com>
2024-11-28 23:08:46 -0500

    portafolio finalizado

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': '/src',  // Resuelve el alias "@" a la carpeta src
+      },
+    },
+    ssr: {
+      noExternal: ['@fontsource-variable/onest'], // Agrega el paquete problemático aquí
+    },
+  },
+});
