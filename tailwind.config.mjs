9611eafafc4f185c8fb23d74565305fffc37b1c8
Rignu1503 <rigoberto.miranda1503@gmail.com>
2026-05-12 22:30:12 -0500

    update porfolio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
+import flowbite from 'flowbite/plugin'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js',
+	],
 	darkMode: 'class',
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [flowbite],
 }
